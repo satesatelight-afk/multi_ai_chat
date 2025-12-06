f2e5cef2434875fdaab10da800bab74588e1eb97
satesatelight-afk <satesatelight.gmail.com>
2025-12-06 22:44:48 +0900

    chatの基本デザイン作成

---
+++ README.md
@@ -0,0 +1,19 @@
+# 実行方法について
+
+```powershell
+cd mutli-ai-chat-app
+npm install
+npm run tauri android init
+```
+
+For Desktop development, run:
+
+```
+npm run tauri dev
+```
+
+For Android development, run:
+
+```
+npm run tauri android dev
+```
---
+++ mutli-ai-chat-app/src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import { invoke } from "@tauri-apps/api/core";
+
+  import { Button } from "$lib/components/ui/button/index.js";
+  import { Textarea } from "$lib/components/ui/textarea/index.js";
+
+  let name = $state("");
+</script>
+
+<div class="flex flex-col h-[calc(100vh-80px)]">
+  <div class="flex-1"></div>
+  <div class="flex flex-col">
+    <Textarea rows={5} maxlength={5}></Textarea>
+    <div class="flex justify-end">
+      <Button class="mt-2">ask</Button>
+    </div>
+  </div>
+</div>
